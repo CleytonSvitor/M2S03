c2b64b26d45fd947c27deee5ab7baca35bfe0373
Cleyton S. Vitor <cleyton.svitor@gmail.com>
2023-07-31 22:01:21 -0300

    M2S02 Ex5 - Concluido

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 const http = require("http")
 const fs = require("fs")
 
-function ListarProdutos(){
+function ListarProdutos() {
     try {
-      const dados = JSON.parse(fs.readFileSync("dados.json", "utf-8"))
-      return JSON.stringify(dados.produtos)
-    } catch (erro){
-      return "Não foi possivel executar a função."
+        const dados = JSON.parse(fs.readFileSync("dados.json", "utf-8"))
+        return JSON.stringify(dados.produtos)
+    } catch (erro) {
+        return "Não foi possivel executar a função."
     }
-  }
+}
 
-const servidor = http.createServer((request, response) => {    
-    switch(request.method){
-        case "GET":      
-          break
-        case "POST":       
-          break       
-      }
- })
-    
-  servidor.listen(3000)
-  console.log("Servidor rodando na porta 3000")
+const servidor = http.createServer((request, response) => {
+    if(request.url == "/produto"){
+        switch (request.method) {
+            case "GET":
+                response.writeHead(200, { "Content-Type": "application/json; charset: utf-8;" })
+                response.end(ListarProdutos())
+                break
+            case "POST":
+                break
+        }
+    }
+})
+servidor.listen(3000)
+console.log("Servidor rodando na porta 3000")
