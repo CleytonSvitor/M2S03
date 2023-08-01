6dee06611b5b2f96cf27a27e586016cf044f8c23
Cleyton S. Vitor <cleyton.svitor@gmail.com>
2023-07-31 21:37:03 -0300

    M2S02 Ex4 - Concluido

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const http = require("http")
 const fs = require("fs")
 
+function ListarProdutos(){
+    try {
+      const dados = JSON.parse(fs.readFileSync("dados.json", "utf-8"))
+      return JSON.stringify(dados.produtos)
+    } catch (erro){
+      return "Não foi possivel executar a função."
+    }
+  }
+
 const servidor = http.createServer((request, response) => {    
     switch(request.method){
         case "GET":      
