286c8a1e8e62cab305442f5529f4c6e456de2989
Cleyton S. Vitor <cleyton.svitor@gmail.com>
2023-07-31 21:22:18 -0300

    M2S02 Ex2 - Concluido

---
+++ index.js
@@ -0,0 +1,9 @@
+const http = require("http")
+const fs = require("fs")
+
+const servidor = http.createServer((request, response) => {
+ 
+ })
+    
+  servidor.listen(3000)
+  console.log("Servidor rodando na porta 3000")
